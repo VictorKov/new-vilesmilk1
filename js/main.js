7e3553af5176ff9996504ed1545b5a8773cde0cd
VictorKov <kovalskij8@yandex.ru>
2024-05-27 22:42:36 +0300

    почти закончил главную

--- js/main.js
+++ js/main.js
@@ -1,13 +1,27 @@
 'use strict'
 
 var swiper = new Swiper(".mySwiper", {
-    autoplay: {
-        delay: 6500,
-        disableOnInteraction: false,
-      },
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-    loop: true,
-  });
+  autoplay: {
+    delay: 6500,
+    disableOnInteraction: false,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  loop: true,
+});
+
+var swiper_1 = new Swiper(".mySwiper-1", {
+  grabCursor: true,
+
+  autoplay: {
+    delay: 6500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  loop: true,
+});
